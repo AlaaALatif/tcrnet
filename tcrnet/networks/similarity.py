72a6d4a2f4f9f3360a892840ad423bff2837d075
Alaa Abdellatif <al.a.latif.94@gmail.com>
2024-04-08 19:38:54 -0700

    Modify to enable handling of user-defined clonotype definition

--- tcrnet/networks/similarity.py
+++ tcrnet/networks/similarity.py
@@ -8,49 +8,39 @@ from tcrdist.public import _neighbors_fixed_radius
 
 
 def compute_tcrdist(qtcr_df: pd.DataFrame,
+                    clonotype_definition: list=['cdr1', 'cdr2', 'cdr3'],
                     num_cpus: int=5):
     ntcr_df = qtcr_df.rename(columns = {
-    'num_records': 'count',
-    'pct_records': 'frequency',
-    'beta_cdr1': 'cdr1_b_aa',
-    'beta_cdr2': 'cdr2_b_aa',
-    'beta_cdr3': 'cdr3_b_aa',
-    'alpha_cdr1': 'cdr1_a_aa',
-    'alpha_cdr2': 'cdr2_a_aa',
-    'alpha_cdr3': 'cdr3_a_aa',
-    'clonotype_id': 'clone_id'
-    }).copy()
-    target_columns = ['count', 'frequency', 
-                    'cdr1_b_aa','cdr2_b_aa', 'cdr3_b_aa', 
-                    'cdr1_a_aa','cdr2_a_aa', 'cdr3_a_aa', 
-                    'clone_id', 'sample_id']
+        'num_records': 'count',
+        'pct_records': 'frequency',
+        'clonotype_id': 'clone_id'
+        }).copy()
+    alpha_cdr_fields = {
+        f'alpha_{sequence_name}': f'{sequence_name}_a_aa' for sequence_name in clonotype_definition
+        }
+    ntcr_df = ntcr_df.rename(columns=alpha_cdr_fields).copy()
+    beta_cdr_fields = {
+        f'beta_{sequence_name}': f'{sequence_name}_b_aa' for sequence_name in clonotype_definition
+        }
+    all_field_names = list(alpha_cdr_fields.values()) + list(beta_cdr_fields.values())
+    ntcr_df = ntcr_df.rename(columns=beta_cdr_fields).copy()
+    target_columns = ['count', 'frequency']
+    target_columns += all_field_names
+    target_columns += ['clone_id', 'sample_id']
     ntcr_df = ntcr_df[target_columns].copy()
     # assign distance metrics and weighting for each TCR sequence
     beta_metrics = {
-        "cdr3_b_aa": pw.metrics.nb_vector_tcrdist,
-        "cdr2_b_aa": pw.metrics.nb_vector_tcrdist,
-        "cdr1_b_aa": pw.metrics.nb_vector_tcrdist,
-        "cdr3_a_aa": pw.metrics.nb_vector_tcrdist,
-        "cdr2_a_aa": pw.metrics.nb_vector_tcrdist,
-        "cdr1_a_aa": pw.metrics.nb_vector_tcrdist
-    }
-
+        cdr_field_name: pw.metrics.nb_vector_tcrdist for cdr_field_name in all_field_names
+        }
+    # print(f"Beta metrics: {beta_metrics}")
     beta_weights = {
-        "cdr3_b_aa": 3,
-        "cdr2_b_aa": 1,
-        "cdr1_b_aa": 1,
-        "cdr3_a_aa": 3,
-        "cdr2_a_aa": 1,
-        "cdr1_a_aa": 1
-    }
+        cdr_field_name: 3 if 'cdr3' in cdr_field_name else 1 for cdr_field_name in all_field_names
+        }
+    # print(f"Beta weights: {beta_weights}")
     beta_kargs = {
-        "cdr3_b_aa": {"use_numba": True},
-        "cdr2_b_aa": {"use_numba": True},
-        "cdr1_b_aa": {"use_numba": True},
-        "cdr3_a_aa": {"use_numba": True},
-        "cdr2_a_aa": {"use_numba": True},
-        "cdr1_a_aa": {"use_numba": True}
-    }
+        cdr_field_name: {"use_numba": True} for cdr_field_name in all_field_names
+        }
+    # print(f"Beta kargs: {beta_kargs}")
     # compute similarity measures using tcrdist
     distance_dict = tcrdist.rep_funcs._pws(df = ntcr_df,
                         metrics = beta_metrics,
